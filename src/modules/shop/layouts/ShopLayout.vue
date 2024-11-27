a23f492f60fe5d39afb0cad425b28827cc11b019
Edgar Vergara <edgarvm13@hotmail.com>
2024-11-27 13:17:09 +0100

    💄 shop layout

---
+++ src/modules/shop/layouts/ShopLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- Header Navbar -->
+  <top-menu />
+
+  <router-view />
+
+  <!-- Footer -->
+  <FooterComponent />
+</template>
+
+<script lang="ts" setup>
+import TopMenu from '../components/TopMenu.vue';
+import FooterComponent from '../components/FooterComponent.vue';
+</script>
